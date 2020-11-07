48ecf59d43b98a60f4342922c14e238d2a4b22d3
zheng3131 <zheng3131@qq.com>
2020-11-08 07:56:34 +0800

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,10 +1,19 @@
 ﻿while true do 
-print("测试")
-touchDown(1, 200, 200)
+print("ks")
+
+x, y = findColor({0, 0, 1079, 2311}, 
+"0|0|0x00dd60,5|20|0xfffcfd,26|1|0x05cb5a,49|35|0xffffff,57|29|0xacf4ce,5|75|0x05c963,62|0|0x03d769",
+95, 0, 0, 0)
+if x > -1 then
+touchDown(1, x, y)
 mSleep(50)
-touchMove(1,200, 200)
+touchMove(1, x, y)
 mSleep(50)
-touchUp(1, 200, 200)  
+touchUp(1, x, y)  
+end
+print("js")
 mSleep(2000)
+
+
 end
 
